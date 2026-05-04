5be8d56224caf02c9bf6022f2b5fbdf7ceb99be6
NikoKavtaradze03 <nikokav7@gmail.com>
2026-05-04 14:11:10 +0400

    Refactor
    Extracted profile header from ProfilePage.tsx

--- realworld-frontend/src/features/profile/pages/ProfilePage.tsx
+++ realworld-frontend/src/features/profile/pages/ProfilePage.tsx
@@ -4,9 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 import PageContainer from "@/components/layout/PageContainer";
 import { queryKeys } from "@/lib/queryKeys";
 import { getProfile } from "../api/profileApi";
-import UserAvatar from "@/components/shared/UserAvatar";
-import FollowButton from "../components/FollowButton";
 import { ProfileTabs } from "../components/ProfileTabs";
+import { ProfileHeader } from "../components/ProfileHeader";
 
 function ProfilePage() {
   const { username } = useParams({ from: "/profile/$username" });
@@ -49,26 +48,7 @@ function ProfilePage() {
   return (
     <main>
       <PageContainer>
-        <section className=" relative rounded-2xl border border-(--color-border) bg-(--color-surface-elevated) p-6 flex flex-col items-center text-center align-center gap-2">
-          <UserAvatar
-            username={profile.username}
-            size="large"
-            image={profile.image}
-          />
-          <h1 className="text-3xl font-semibold text-(--color-text)">
-            {profile.username}
-          </h1>
-
-          <p className="mt-2 text-sm text-(--color-text-secondary) max-w-200">
-            {profile.bio ?? "No bio yet."}
-          </p>
-          <div className="flex justify-end w-full mt-2">
-            <FollowButton
-              username={profile.username}
-              following={profile.following}
-            ></FollowButton>
-          </div>
-        </section>
+        <ProfileHeader profile={profile} />
         <ProfileTabs profile={profile} />
       </PageContainer>
     </main>
